880d999b24ec5184f2e6702d284d44e6525efbc9
ChooJG <cnwprms@naver.com>
2024-11-29 00:08:09 +0900

    test 기본 jpa repository CRUD 검증

---
+++ src/main/java/styudy/data_jpa/repository/MemberRepository.java
@@ -0,0 +1,4 @@
+package styudy.data_jpa.repository;
+
+public interface MemberRepository {
+}
--- src/test/java/styudy/data_jpa/entity/MemberTest.java
+++ src/test/java/styudy/data_jpa/entity/MemberTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
-@Rollback(value = false)
+//@Rollback(value = false)
 @SpringBootTest
 class MemberTest {
 
